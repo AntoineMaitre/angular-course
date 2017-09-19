d37a6c4bb00a7d389a1c24e53cbe8c1154786ed7
Antoine Maitre <ant.maitre@gmail.com>
2017-09-19 14:36:35 +0200

    Angular fundamentals: Exercice 1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CoursesComponent} from './courses.component';
+import {CourseComponent} from './course/course.component';
+import {CoursesService} from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import {AuthorsService} from './authors/authors.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CoursesService,
+    AuthorsService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
