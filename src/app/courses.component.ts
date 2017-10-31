a33d6c72b16eb94f4949e85fb48affa03e68a6e5
Antoine Maitre <ant.maitre@gmail.com>
2017-10-31 01:25:24 +0100

    Angular fundamentals: Exercice 2

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,20 +4,36 @@ import {CoursesService} from './courses.service';
 @Component({
   selector: 'courses',
   template: `
-    <h2>{{ title }}</h2>
-    <ul>
-      <li *ngFor="let course of courses">
-        {{ course }}
-      </li>
-    </ul>
+    {{ text | summary:10 }}
+    <div (click)="onFavorite()" class="glyphicon glyphicon-star"
+         [ngClass]="{ 'glyphicon-star-empty': !isFavorite() }">
+    </div>
   `
+  // template:
+  //   <h2>{{ title }}</h2>
+  //   <ul>
+  //     <li *ngFor="let course of courses">
+  //       {{ course }}
+  //     </li>
+  //   </ul>
+  // `
 })
 
 export class CoursesComponent {
   title = 'List of courses';
   courses;
+  text = `Lorem ipsum is simply dummy text of the printing typescript`;
+  public isFavoriteElem = false;
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
+  }
+
+  onFavorite() {
+    this.isFavoriteElem = !this.isFavoriteElem;
+  }
+
+  isFavorite(): boolean {
+    return this.isFavoriteElem;
   }
 }
